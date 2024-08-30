95ad944c0120c0187dff095a27f6ff6c50c63399
Ajay Kanel <vikashkanelbhai@gmail.com>
2024-08-30 23:26:10 +0530

    count user number

---
+++ user_no_count.php
@@ -0,0 +1,73 @@
+<html>
+<head>
+<style>
+form{background:pink;width:400px; padding-top:10px; padding-left:20px;}
+form input{height:30px; border-radius:5px;}
+.box{display:flex;  }
+.box1{background:blue; 
+margin:10px; 
+height:250px;  
+width:100px; 
+text-align:center;}
+</style>
+</head>
+<body>
+<form method="post">
+<input type="text" name="num" value="" required>
+<input type="submit" name="ok1" value="while" >
+<input type="submit" name="ok2" value="do_while" >
+<input type="submit" name="ok3" value="for_loop" >
+<div class="box">
+	<div class="box1">
+	<p>while_loop</p>
+	<?php
+	if(isset($_POST['ok1']))
+	{
+	$num=$_POST['num'];	
+	$i=1;
+	$sum=0;
+	while($i<=$num)
+	{
+		$sum+=$i;
+		echo "<br>".$sum;
+		$i++;
+	}
+	}?>
+	</div>
+	<div class="box1">
+	<p>do_while</p>
+	<?php 
+	if(isset($_POST['ok2']))
+	{
+	$num=$_POST['num'];
+	$j=1;
+	$sum=0;
+	do
+	{
+		$sum+=$j;
+		echo "<br>".$sum;
+		$j++;
+	}
+	while($j<=$num);
+	}
+	?>
+	</div>
+	<div class="box1">
+	<p>for_while</p>
+	<?php
+	if(isset($_POST['ok3']))
+	{
+		$sum=0;
+		$num=$_POST['num'];
+	for($k=1; $k<=$num; $k++)
+	{
+		
+		$sum+=$k;
+		echo "<br>".$sum;
+	}
+	}?>
+	</div>
+</div>
+</form>
+</body>
+</html>
