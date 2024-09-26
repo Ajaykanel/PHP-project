f8dc4f4c1afff8e58e9d403de24f4358bfe1ce99
Ajay Kanel <vikashkanelbhai@gmail.com>
2024-09-26 22:59:41 +0530

    arithmetic op

--- Arithmetic_operation.php
+++ Arithmetic_operation.php
@@ -5,24 +5,44 @@ if(isset($_POST['submit']))
 	$b=$_POST['b'];
 	$op=$_POST['op'];
 	
-	$result=(($op=='+')*($a+$b)+($op=='-')*($a-$b)+($op=='*')*($a*$b)+($op=='/')*($a/$b));
-		
+$result=(($op=='+')*($a+$b)+($op=='-')*($a-$b)+($op=='*')*($a*$b)+($op=='/')*($a/$b));
 }
 ?>
 <html>
 <head>
 <style>
-#box1{width:115px;}
+
+
+fieldset{height:300px; width:400px; margin-left:30%; background-color:pink; 
+}
+legend{color:red; text-align:center; font-size:30px; }
+.box{color:red; font-size:20px; height:50px; text-align:center;}
+.box2{color:red;}
+.boxbt{font-size:50px;}
 </style>
 </head>
-<body><pre> <fieldset><legend>operation_arithmetic</legend>
+<body><pre> <fieldset><legend>Arithmetic Operators</legend>
 <form method='post' name="no">
-	First No:  <input type="text" value="" name="a" placeholder="Enter value a">
-	Symbol:	   <input type="text" value="" name="op">
-	Secound No:<input type="text" value="" name="b" placeholder="Enter value b">
-		   <input type="submit" value="submit" name="submit"><input type="text" value="<?=$result?>" id="box1">
-	
+<table border="1" cellpadding="0" >
+<tr>
+<td class="box">First No:</td>
+<td class="box2"><input type="text" value="" name="a" class="box" placeholder="Enter value a"></td>
+</tr>
 
+<tr>
+<td class="box">Symbol:</td>
+<td><input type="text" value="" name="op" class="box"></td>
+</tr>
+
+<tr>
+<td class="box">Secound No:</td>
+<td><input type="text" value="" name="b" class="box" placeholder="Enter value b"></td>
+</tr>
+
+<tr  >
+<td ><input type="submit"  value="submit" name="submit" class="boxbt"></td><td><input type="text" value="<?=$result?>" class="box"></td>
+</tr>
+</table>
 </form>
 </fieldset>
 </pre>
